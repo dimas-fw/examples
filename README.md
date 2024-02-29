ed39caf2367f69415f9766a638946a61f2c6cb28
stepkun <stephan.kunz@kabelbw.de>
2024-02-29 18:14:05 +0100

    new repo for examples & benchmarks

---
+++ README.md
@@ -0,0 +1,4 @@
+# Examples
+
+## [Montblanc](https://github.com/dimas-fw/examples/tree/main/montblanc)
+The montblanc benchmark simulates a robot using several nodes/agents communicating with a workstation that is running also several nodes/agents.
---
+++ montblanc/lyon/src/main.rs
@@ -0,0 +1,36 @@
+// Copyright © 2024 Stephan Kunz
+
+//! The node 'lyon' subscribes to a Float32 on the topic /amazon and publishes the received value on topic /tigris
+//!
+//! This source is part of `DiMAS` implementation of Montblanc benchmark for distributed systems
+
+use dimas::prelude::*;
+use tracing::info;
+
+#[derive(Debug)]
+struct AgentProps {}
+
+fn amazon_callback(ctx: &ArcContext<AgentProps>, message: &Message) {
+	let value: messages::Float32 = bitcode::decode(message).expect("should not happen");
+	let _ = ctx.put_with("tigris", &value);
+	info!("sent: '{value}'");
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	tracing_subscriber::fmt::init();
+
+	let properties = AgentProps {};
+	let mut agent = Agent::new(Config::local(), properties);
+
+	agent.publisher().msg_type("tigris").add()?;
+
+	agent
+		.subscriber()
+		.put_callback(amazon_callback)
+		.msg_type("amazon")
+		.add()?;
+
+	agent.start().await;
+	Ok(())
+}
